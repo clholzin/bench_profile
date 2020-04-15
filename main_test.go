5ac49475664bab055dd1a25ccd5a90b57e153137
Craig Holzinger <clholzin@gmail.com>
2020-04-15 14:29:03 -0700

    coverage slides

--- main_test.go
+++ main_test.go
@@ -4,19 +4,51 @@ import (
 	"context"
 	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
+	"os"
+	"reflect"
 	"strings"
 	"testing"
 	"time"
 )
 
 func init() {
-	go Start()
+	//go Start()
 	time.Sleep(time.Second * 3)
 
 }
 
-func BenchmarkFoo(b *testing.B) {
+func TestMain(m *testing.M) {
+	// setup before tests
+	go main()
+	os.Exit(m.Run())
+	// teardown after tests
+}
 
+func TestFoo(t *testing.T) {
+	httpStat := NewStat()
+	req := httptest.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	fooHandler := httpStat.Foo()
+	fooHandler(w, req)
+	resp := w.Result()
+	expected := 200
+	if resp.StatusCode != expected {
+		t.Errorf("Got %d want %d", resp.StatusCode, expected)
+	}
+}
+
+func TestWork(t *testing.T) {
+	var worker = work(context.Background(), 20)
+	t.Log(reflect.TypeOf(worker).Kind().String())
+	if reflect.TypeOf(worker).Kind().String() != "func" {
+		t.Logf("worker was not type func")
+	}
+}
+
+func BenchmarkFoo(b *testing.B) {
+	go Start()
+	time.Sleep(time.Second * 3)
 	count := 0
 
 	for i := 0; i < b.N; i++ {
